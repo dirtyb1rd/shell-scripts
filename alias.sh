f48fb71db1f325b2d99d2c1bc46c43a325505380
dirtyb1rd <work@benparis.net>
2024-01-25 23:12:13 -0800

    Update 2

--- alias.sh
+++ alias.sh
@@ -2,7 +2,7 @@
 
 #Written by Ben Paris
 
-#Create custom commands in both bash and zsh
+#Create custom commands in both bash and zsh at the same time
 
 #!/bin/bash
 
@@ -14,9 +14,11 @@ do
 
 #Gather and add command
 	echo "Please type in the custom command you wish to creat. (i.e. setupmyvpn, startupscript, closeapps, etc.)"
+	read name
+	echo "Please type in the full command with all arguments"
 	read cmd
-	echo $cmd >> ~/.bashrc
-	echo $cmd >> ~/.zshrc
+	echo "alias "$name"='"$cmd"'" >> ~/.bashrc
+	echo "alias "$name"='"$cmd"'" >> ~/.zshrc
 #Prompt for repeat
         echo -n "Do you have another custom command you wish to create? (Y/N)"
         read -n1 Input
--- thm-alias.sh
+++ thm-alias.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+#By Ben Paris
+
 #TryHackMe VPN Setup
+
+#Check for download
 echo 'Do you have a TryHackMe.com VPN profile to set up? Answer (y/n) (If so please make sure it is in your downloads folder.)'
 read vpn
 if [[ $vpn =~ ^[Yy]$ ]];
 then
+
+#Get name in order to find file
 echo 'Hello, please provide THM username, this will be user to find your THM vpn profile in your downloads folder at ~/Downloads/^USERNAME^.ovpn'
 read thm
+
+#Make dir. and move vpn file into it
 mkdir ~/Documents/thmvpn
 mv ~/Downloads/$thm.ovpn ~/Documents/thmvpn/$thm.ovpn
+
+#Add connection command to bash adn zsh rc file
 echo "alias thmvpn='sudo openvpn ~/Documents/"$thm".ovpn'" >> ~/.bashrc
 echo "alias thmvpn='sudo openvpn ~/Documents/"$thm".ovpn'" >> ~/.zshrc
 echo 'Your VPN alias is now setup, in order to connect, do the following: type thmvpn into the command line in a bash or zshell and enter password when prompted.'
+
+#If not
 else
 echo 'Please run this script again when you have your THM VPN profile in your Downloads folder. Goodbye.'
 fi
