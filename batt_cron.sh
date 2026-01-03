b37f7baad762daf82172a9c7b5be8c8c73caf738
dirtyb1rd <work@benparis.net>
2026-01-03 15:21:21 -0800

    fix: failed dbus message with fyi

--- batt_cron.sh
+++ batt_cron.sh
@@ -3,8 +3,16 @@
 STATUS=$(cat /sys/class/power_supply/BAT0/status)
 CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
 
+# Detect and export the necessary variables for GUI notifications
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
+
+# (Optional) Sometimes needed for specific notification daemons
+export DISPLAY=:0
+
 if [ "$CHARGE" -lt "20" ] && [ "$STATUS" = "Discharging" ]; then
   fyi "🪫 Battery Low"
+  echo "🪫 Battery Low"
 else
-  :
+  echo "Battery Fine"
 fi
